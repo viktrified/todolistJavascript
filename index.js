42d10d63c5787d2c5f824405031db8b138ba9ba4
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-11-04 09:33:41 +0100

    minor changes

---
+++ index.js
@@ -0,0 +1,8 @@
+// add activity/activities to list
+// remove activity/activities from list
+// tick completed activity/activities
+// untick activity/activities
+
+
+
+// document.getElementById("activity-list").textContent;
